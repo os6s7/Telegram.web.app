e3318d5c3647cc01faf996dde6897c29445c8624
Os6s77 <125276329+os6s7@users.noreply.github.com>
2025-07-26 03:40:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'web_app')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'web_app', 'index.html'));
 });
 
